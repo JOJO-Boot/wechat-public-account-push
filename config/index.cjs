d09d3b39eb8dde91ce4963c1e835f30a37611b3a
JOJO-Boot <65119770+JOJO-Boot@users.noreply.github.com>
2022-09-26 14:29:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,11 +63,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '大哥',
+      name: '老婆',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '	5w_6gUco9ptI7ESMsMsq-zNnA2AQ_MEwkXiQg-7hqeE',
+      id: 'oUVyC6oZsES2KJbXOhC-omj1OXR0',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '5w_6gUco9ptI7ESMsMsq-zNnA2AQ_MEwkXiQg-7hqeE',
       // 所在省份
       province: '河南',
       // 所在城市
